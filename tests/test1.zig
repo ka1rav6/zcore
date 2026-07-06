bb80fac17350183cb29f6f58e6175517189a89cd
Kairav <kairavdutta@gmail.com>
2026-07-06 23:03:38 +0530

    started using std.testing.allocator instead of heap.page_allocator

--- tests/test1.zig
+++ tests/test1.zig
@@ -2,7 +2,7 @@ const Tensor = @import("zcore").Tensor;
 const std = @import("std");
 
 test "completeTest1" {
-    var myTensor = try Tensor(i8).init(std.heap.page_allocator, &[_]usize{ 3, 3 });
+    var myTensor = try Tensor(i8).init(std.testing.allocator, &[_]usize{ 3, 3 });
     defer myTensor.destroy(); // destroys it at the end of the test
     myTensor.fill(10);
     try std.testing.expectEqual(@as(i8, 10), (try myTensor.get(&[_]usize{ 2, 2 })).*);
