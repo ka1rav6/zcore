94db78fd317e7967684591e10ad9634e1016dbe4
Kairav <kairavdutta@gmail.com>
2026-07-06 14:41:13 +0530

    added indexing to tensors using the .get() method

--- src/tensor/tensor.zig
+++ src/tensor/tensor.zig
@@ -38,7 +38,7 @@ fn numElements(shape: []const usize) usize{
 /// Each Tensor is also given its own allocator
 pub fn Tensor(comptime T: type) type {
     return struct {
-        // properties:
+        // -------------- members ---------------
         _allocator   : std.mem.Allocator,
         _data        : []T              ,
         _shape       : []usize          ,
@@ -46,6 +46,8 @@ pub fn Tensor(comptime T: type) type {
         _owns_memory : bool = true      ,
         
         const Self = @This(); // so methods can directly use 'Self' instead of @This() everywhere
+        
+        // --------------------- constructors and destructors --------------------------
 
         /// The constructor for the tensor struct
         /// requires allocator and shape as input
@@ -73,6 +75,28 @@ pub fn Tensor(comptime T: type) type {
                 self._allocator.free(self.data);
             self._allocator.free( self._shape );
             self._allocator.free(self._strides);
+        }
+
+        // ---------------------------- other methods -------------------------------------
+
+        /// converts multi-dimensional indices into a single index for the flattened _data array
+        fn offset(self : Self, indices: []const usize) usize {
+            // EXPLAINATION :
+            // user wants : [1, 2]
+            // but we have flattened array stored
+            // so it converts it into the index we want
+
+            std.debug.assert(indices.len == self._shape.len);
+            var off:usize = 0;
+            for (indices, 0..) |idx, i| {
+                off += idx * self._strides[i]; // standard row major calculation
+            }
+            return off;
+        }
+        /// returns the value of the index passed by the user
+        /// internally calls the offset function to get the index in row-major format
+        pub fn get(self: Self, indices: []const usize) *T{
+            return &self._data[self.offset(indices)];
         }
     };
 }
